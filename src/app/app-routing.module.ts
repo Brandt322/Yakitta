30f29ffb28d67b801373519a859b387b83fc494e
Brandt322 <bgamboaa98@gmail.com>
2024-05-22 02:08:01 -0500

    feat: Added header and login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   {
-    path: 'main', component: MainComponent
+    path: 'productos', component: MainComponent
   },
 ];
 
--- src/app/shared/components/layout/header/header.component.ts
+++ src/app/shared/components/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, HostListener } from '@angular/core';
 import { createPopper } from '@popperjs/core';
 
 @Component({
@@ -9,25 +9,49 @@ import { createPopper } from '@popperjs/core';
 export class HeaderComponent implements AfterViewInit {
   showMenu = false;
   dropdownPopoverShow = false;
+  cartDropdownShow = false;
+  private clickOutsideListener: any;
 
   @ViewChild('btnDropdownRef', { static: false }) btnDropdownRef!: ElementRef;
+  @ViewChild('btnCartDropdownRef', { static: false }) btnCartDropdownRef!: ElementRef;
+
   @ViewChild('popoverDropdownRef', { static: false }) popoverDropdownRef!: ElementRef;
+  @ViewChild('cartDropdownRef', { static: false }) cartDropdownRef!: ElementRef;
+
+  constructor(private eRef: ElementRef) { }
 
   ngAfterViewInit(): void {
     createPopper(this.btnDropdownRef.nativeElement, this.popoverDropdownRef.nativeElement, {
       placement: 'bottom'
     });
+    createPopper(this.btnCartDropdownRef.nativeElement, this.cartDropdownRef.nativeElement, {
+      placement: 'bottom'
+    });
   }
 
+  mouseLeaveButton(event: MouseEvent) {
+    if (!this.popoverDropdownRef.nativeElement.contains(event.relatedTarget)) {
+      this.dropdownPopoverShow = false;
+    }
+  }
 
   toggleTooltip() {
-    // Aquí puedes definir lo que quieres que suceda cuando se dispare el evento
-    // Por ejemplo, puedes cambiar el valor de dropdownPopoverShow
     this.dropdownPopoverShow = !this.dropdownPopoverShow;
+  }
+
+  toggleCartDropdown() {
+    this.cartDropdownShow = !this.cartDropdownShow;
   }
 
   toggleNavbar() {
     this.showMenu = !this.showMenu;
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickout(event: any) {
+    if (!this.eRef.nativeElement.contains(event.target)) {
+      this.cartDropdownShow = false;
+    }
   }
 
 }
